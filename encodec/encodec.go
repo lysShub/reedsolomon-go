725dc1e15cf57babe5c31e77c555deac60a31a2f
lysShub <lysShub@gmail.com>
2026-09-07 23:23:15 +0800

    fix

--- encodec/encodec.go
+++ encodec/encodec.go
@@ -37,7 +37,7 @@ func encodeMatrixBuf(dst, buf []byte, g, d int) int {
 	for i := range idxs[:d] {
 		idxs[i] = i
 	}
-	rows := delRows(dst, g, idxs[:d]...)
+	rows := delRows(dst[:g*d], g, idxs[:d]...)
 	return rows * d
 }
 
@@ -64,7 +64,7 @@ func decodeMatrixBuf(dst, buf []byte, g, d int, indexs []uint8) int {
 			delN++
 		}
 	}
-	n := delRows(dst, g, del[:delN]...)
+	n := delRows(dst[:g*d], g, del[:delN]...)
 
 	inv := buf[g*d : g*d+d*d]
 	work := buf[g*d+2*d*d : g*d+4*d*d]
