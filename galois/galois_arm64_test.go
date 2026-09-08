e00bef5bb022d039a13e10612040c355e3b58efa
lysShub <lysShub@gmail.com>
2026-09-08 22:37:54 +0800

    update bench

--- galois/galois_arm64_test.go
+++ galois/galois_arm64_test.go
@@ -1,5 +1,5 @@
-//go:build amd64
-// +build amd64
+//go:build arm64
+// +build arm64
 
 package galois
 
@@ -8,14 +8,9 @@ import (
 	"crypto/rand"
 	"fmt"
 	"testing"
-
-	"golang.org/x/sys/cpu"
 )
 
 func Test_Base(t *testing.T) {
-	if !cpu.X86.HasAVX512GFNI {
-		t.Skip("AVX512GFNI not supported")
-	}
 	var crossSizes = []int{0, 1, 15, 16, 17, 31, 63, 64, 65, 127, 128, 129, 255, 256, 257, 511, 1500}
 
 	t.Run("mux", func(t *testing.T) {
@@ -24,10 +19,10 @@ func Test_Base(t *testing.T) {
 				i := make([]byte, n)
 				rand.Read(i)
 				oGo := make([]byte, n)
-				oGfni := make([]byte, n)
+				oSmid := make([]byte, n)
 				mulVect_go(byte(c), i, oGo)
-				mulVect_gfni(byte(c), i, oGfni)
-				if !bytes.Equal(oGo, oGfni) {
+				mulVect_smid(byte(c), i, oSmid)
+				if !bytes.Equal(oGo, oSmid) {
 					t.Fatalf("c=%d n=%d", c, n)
 				}
 			}
@@ -41,12 +36,12 @@ func Test_Base(t *testing.T) {
 				rand.Read(i)
 				rand.Read(o)
 				oGo := make([]byte, n)
-				oGfni := make([]byte, n)
+				oSmid := make([]byte, n)
 				copy(oGo, o)
-				copy(oGfni, o)
+				copy(oSmid, o)
 				mulXorVect_go(byte(c), i, oGo)
-				mulXorVect_gfni(byte(c), i, oGfni)
-				if !bytes.Equal(oGo, oGfni) {
+				mulXorVect_smid(byte(c), i, oSmid)
+				if !bytes.Equal(oGo, oSmid) {
 					t.Fatalf("c=%d n=%d", c, n)
 				}
 			}
@@ -67,7 +62,6 @@ const benchMulC byte = 0b10101010
 const searchByte byte = 0b10101010
 
 func Benchmark_mulVect(b *testing.B) {
-
 	for _, n := range benchSizes {
 		b.Run(fmt.Sprintf("go/%d", n), func(b *testing.B) {
 			i, o := make([]byte, n), make([]byte, n)
@@ -84,18 +78,6 @@ func Benchmark_mulVect(b *testing.B) {
 			b.ResetTimer()
 			for range b.N {
 				mulVect_smid(benchMulC, i, o)
-			}
-		})
-
-		b.Run(fmt.Sprintf("gfni/%d", n), func(b *testing.B) {
-			if !cpu.X86.HasAVX512GFNI {
-				b.Skip("AVX512GFNI not supported")
-			}
-			i, o := make([]byte, n), make([]byte, n)
-			b.SetBytes(int64(n))
-			b.ResetTimer()
-			for range b.N {
-				mulVect_gfni(benchMulC, i, o)
 			}
 		})
 		fmt.Println()
@@ -120,18 +102,6 @@ func Benchmark_mulXorVect(b *testing.B) {
 			b.ResetTimer()
 			for range b.N {
 				mulXorVect_smid(benchMulC, i, o)
-			}
-		})
-
-		b.Run(fmt.Sprintf("gfni/%d", n), func(b *testing.B) {
-			if !cpu.X86.HasAVX512GFNI {
-				b.Skip("AVX512GFNI not supported")
-			}
-			i, o := make([]byte, n), make([]byte, n)
-			b.SetBytes(int64(n))
-			b.ResetTimer()
-			for range b.N {
-				mulXorVect_gfni(benchMulC, i, o)
 			}
 		})
 		fmt.Println()
